e796d3ff8dbbb34e485fea5691554b0c954cb19a
Gabriel <gaha204@hotmail.com>
2024-08-07 19:50:04 -0300

    add unrolled_naive_intersect

--- src/roaringish.rs
+++ src/roaringish.rs
@@ -505,15 +505,11 @@ impl<'a> BorrowRoaringishPacked<'a> {
                     doc_id_groups_result
                         .get_unchecked_mut(i)
                         .write(*lhs_doc_id_groups);
+                    let rhs = *rhs_positions.get_unchecked(rhs_i);
+                    rhs_positions_result.get_unchecked_mut(i).write(rhs);
                     if FIRST {
                         let lhs = *lhs_positions.get_unchecked(lhs_i);
                         lhs_positions_result.get_unchecked_mut(i).write(lhs);
-
-                        let rhs = *rhs_positions.get_unchecked(rhs_i);
-                        rhs_positions_result.get_unchecked_mut(i).write(rhs);
-                    } else {
-                        let rhs = *rhs_positions.get_unchecked(rhs_i);
-                        rhs_positions_result.get_unchecked_mut(i).write(rhs);
                     }
                 }
 
@@ -624,6 +620,68 @@ impl<'a> BorrowRoaringishPacked<'a> {
             )
         }
     }
+
+    fn unrolled_naive_intersect<const FIRST: bool>(&self, rhs: &Self) -> (Vec<u64>, Vec<u16>, Vec<u16>) {
+        let lhs_positions = self.positions;
+        let rhs_positions = rhs.positions;
+        let lhs_doc_id_groups = self.doc_id_groups;
+        let rhs_doc_id_groups = rhs.doc_id_groups;
+
+        let mut lhs_i = 0;
+        let mut rhs_i = 0;
+
+        let min = lhs_doc_id_groups.len().min(rhs_doc_id_groups.len());
+        let mut i = 0;
+        let mut doc_id_groups_result: Box<[MaybeUninit<u64>]> = Box::new_uninit_slice(min);
+        let mut lhs_positions_result: Box<[MaybeUninit<u16>]> = if FIRST {
+            Box::new_uninit_slice(min)
+        } else {
+            Box::new_uninit_slice(0)
+        };
+        let mut rhs_positions_result: Box<[MaybeUninit<u16>]> = Box::new_uninit_slice(min);
+
+        unsafe {
+            loop {
+                let lhs_bound = lhs_doc_id_groups.len() - lhs_i;
+                let rhs_bound = rhs_doc_id_groups.len() - rhs_i;
+                let iters = lhs_bound.min(rhs_bound);
+                if iters == 0 {
+                    break;
+                }
+    
+                for _ in 0..iters {
+                    let lhs_doc_id_groups = *lhs_doc_id_groups.get_unchecked(lhs_i);
+                    let rhs_doc_id_groups = *rhs_doc_id_groups.get_unchecked(rhs_i);
+
+                    let rhs = *rhs_positions.get_unchecked(rhs_i);
+                    doc_id_groups_result.get_unchecked_mut(i).write(lhs_doc_id_groups);
+                    rhs_positions_result.get_unchecked_mut(i).write(rhs);
+                    if FIRST {
+                        let lhs = *lhs_positions.get_unchecked(lhs_i);
+                        lhs_positions_result.get_unchecked_mut(i).write(lhs);
+                    }
+                    i += (lhs_doc_id_groups == rhs_doc_id_groups) as usize;
+                    lhs_i += (lhs_doc_id_groups <= rhs_doc_id_groups) as usize;
+                    rhs_i += (rhs_doc_id_groups <= lhs_doc_id_groups) as usize;
+                }
+            }
+        }
+
+        let doc_id_groups_result_ptr = Box::into_raw(doc_id_groups_result) as *mut _;
+        let lhs_positions_result_ptr = Box::into_raw(lhs_positions_result) as *mut _;
+        let rhs_positions_result_ptr = Box::into_raw(rhs_positions_result) as *mut _;
+        unsafe {
+            (
+                Vec::from_raw_parts(doc_id_groups_result_ptr, i, min),
+                if FIRST {
+                    Vec::from_raw_parts(lhs_positions_result_ptr, i, min)
+                } else {
+                    Vec::from_raw_parts(lhs_positions_result_ptr, 0, 0)
+                },
+                Vec::from_raw_parts(rhs_positions_result_ptr, i, min),
+            )
+        }
+    }
 }
 
 impl RoaringishPacked {
@@ -669,15 +727,15 @@ impl RoaringishPacked {
         let rhs = BorrowRoaringishPacked::new(rhs);
         let b = std::time::Instant::now();
         // let (doc_id_groups, lhs_positions, rhs_positions) = lhs.naive_intersect::<true>(&rhs);
+        let (doc_id_groups, lhs_positions, rhs_positions) = lhs.unrolled_naive_intersect::<true>(&rhs);
         // let (doc_id_groups, lhs_positions, rhs_positions) = lhs.gallop_intersect::<true>(&rhs);
-        let (doc_id_groups, lhs_positions, rhs_positions) = unsafe { lhs.simd_intersect::<true, false>(&rhs) };
-        // assert_eq!(doc_id_groups0, doc_id_groups1);
-        // assert_eq!(doc_id_groups0, doc_id_groups);
-        // assert_eq!(lhs_positions0, lhs_positions1);
-        // assert_eq!(lhs_positions0, lhs_positions);
-        // assert_eq!(rhs_positions0, rhs_positions1);
-        // assert_eq!(rhs_positions0, rhs_positions);
-        // println!("{}", doc_id_groups.len());
+        // let (doc_id_groups, lhs_positions, rhs_positions) = unsafe { lhs.simd_intersect::<true, false>(&rhs) };
+
+        assert_eq!(lhs.unrolled_naive_intersect::<true>(&rhs), lhs.naive_intersect::<true>(&rhs));
+        assert_eq!(lhs.gallop_intersect::<true>(&rhs), lhs.naive_intersect::<true>(&rhs));
+        assert_eq!(unsafe { lhs.simd_intersect::<true, true>(&rhs) }, lhs.naive_intersect::<true>(&rhs));
+        assert_eq!(unsafe { lhs.simd_intersect::<true, false>(&rhs) }, lhs.naive_intersect::<true>(&rhs));
+
         stats
             .first_gallop
             .fetch_add(b.elapsed().as_micros() as u64, Relaxed);
@@ -697,13 +755,15 @@ impl RoaringishPacked {
         let msb_packed = BorrowRoaringishPacked::from_raw(&msb_doc_id_groups, &msb_positions);
         let b = std::time::Instant::now();
         // let (msb_doc_id_groups, _, msb_rhs_positions) = msb_packed.naive_intersect::<false>(&rhs);
+        let (msb_doc_id_groups, _, msb_rhs_positions) = msb_packed.unrolled_naive_intersect::<false>(&rhs);
         // let (msb_doc_id_groups, _, msb_rhs_positions) = msb_packed.gallop_intersect::<false>(&rhs);
-        let (msb_doc_id_groups, _, msb_rhs_positions) = unsafe { msb_packed.simd_intersect::<false, false>(&rhs) };
-        // assert_eq!(msb_doc_id_groups0, msb_doc_id_groups1);
-        // assert_eq!(msb_doc_id_groups0, msb_doc_id_groups);
-        // assert_eq!(msb_rhs_positions0, msb_rhs_positions1);
-        // assert_eq!(msb_rhs_positions0, msb_rhs_positions);
-        // println!("{}", msb_doc_id_groups.len());
+        // let (msb_doc_id_groups, _, msb_rhs_positions) = unsafe { msb_packed.simd_intersect::<false, false>(&rhs) };
+
+        assert_eq!(msb_packed.unrolled_naive_intersect::<false>(&rhs), msb_packed.naive_intersect::<false>(&rhs));
+        assert_eq!(msb_packed.gallop_intersect::<false>(&rhs), msb_packed.naive_intersect::<false>(&rhs));
+        assert_eq!(unsafe { msb_packed.simd_intersect::<false, true>(&rhs) }, msb_packed.naive_intersect::<false>(&rhs));
+        assert_eq!(unsafe { msb_packed.simd_intersect::<false, false>(&rhs) }, msb_packed.naive_intersect::<false>(&rhs));
+
         stats
             .second_gallop
             .fetch_add(b.elapsed().as_micros() as u64, Relaxed);
