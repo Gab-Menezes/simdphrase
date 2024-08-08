cf337d32762d2fa7bad15af3acdfff46e652c78a
Gabriel Menezes <gaha204@hotmail.com>
2024-08-08 20:51:58 -0300

    add avx2 to fuzz

--- fuzz/fuzz_targets/vp2intersect.rs
+++ fuzz/fuzz_targets/vp2intersect.rs
@@ -1,10 +1,26 @@
 #![feature(stdarch_x86_avx512)]
+#![feature(portable_simd)]
 
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use std::arch::x86_64::{__m512i, _mm512_loadu_epi64, _mm512_cmpeq_epi64_mask, _mm512_shuffle_epi32, _mm512_alignr_epi32, _MM_PERM_BADC, __mmask8};
+use std::arch::x86_64::{
+    __m512i, 
+    _mm512_loadu_epi64, 
+    _mm512_cmpeq_epi64_mask, 
+    _mm512_shuffle_epi32, 
+    _mm512_alignr_epi32, 
+    _MM_PERM_BADC, 
+    __mmask8,
+    _mm256_loadu_si256,
+    __m256i,
+    _mm256_shuffle_epi32,
+    _mm256_or_si256,
+    _mm256_permute2x128_si256,
+    _mm256_cmpeq_epi64
+};
 use std::arch::asm;
+use std::simd::Mask;
 
 #[cfg(all(
     target_feature = "avx512f",
@@ -12,7 +28,7 @@ use std::arch::asm;
     target_feature = "avx512bw",
     target_feature = "avx512vp2intersect"
 ))]
-unsafe fn native_vp2intersectq(a: __m512i, b: __m512i) -> (__mmask8, __mmask8) {
+unsafe fn native_vp2intersectq_avx512(a: __m512i, b: __m512i) -> (u8, u8) {
     let mut mask0: __mmask8;
     let mut mask1: __mmask8;
     asm!(
@@ -32,7 +48,7 @@ unsafe fn native_vp2intersectq(a: __m512i, b: __m512i) -> (__mmask8, __mmask8) {
     target_feature = "avx512dq",
     target_feature = "avx512bw",
 ))]
-unsafe fn emulate_vp2intersectq(a: __m512i, b: __m512i) -> (__mmask8, __mmask8) {
+unsafe fn emulate_vp2intersectq_avx512(a: __m512i, b: __m512i) -> (u8, u8) {
     let a1 = _mm512_alignr_epi32(a, a, 4);
     let a2 = _mm512_alignr_epi32(a, a, 8);
     let a3 = _mm512_alignr_epi32(a, a, 12);
@@ -60,10 +76,56 @@ unsafe fn emulate_vp2intersectq(a: __m512i, b: __m512i) -> (__mmask8, __mmask8) 
     return (mask0, mask1);
 }
 
+#[cfg(all(
+    target_feature = "avx512f",
+    target_feature = "avx512dq",
+    target_feature = "avx512bw",
+    target_feature = "avx512vp2intersect"
+))]
+unsafe fn native_vp2intersectq_avx2(a: __m256i, b: __m256i) -> (u8, u8) {
+    let mut mask0: __mmask8;
+    let mut mask1: __mmask8;
+    asm!(
+        "vp2intersectq k2, {0}, {1}",
+        in(ymm_reg) a,
+        in(ymm_reg) b,
+        out("k2") mask0,
+        out("k3") mask1,
+        options(pure, nomem, nostack),
+    );
+
+    (mask0, mask1)
+}
+
+unsafe fn emulate_vp2intersectq_avx2(a: __m256i, b: __m256i) -> (u8, u8) {
+    let a1 = _mm256_permute2x128_si256(a, a, 1);
+    let b1 = _mm256_shuffle_epi32(b, _MM_PERM_BADC);
+
+    let m00 = _mm256_cmpeq_epi64(a, b);
+    let m01 = _mm256_cmpeq_epi64(a, b1);
+    let m10 = _mm256_cmpeq_epi64(a1, b);
+    let m11 = _mm256_cmpeq_epi64(a1, b1);
+
+    let l = _mm256_or_si256(m00, m01);
+    let h = _mm256_or_si256(m10, m11);
+    let mask0 = _mm256_or_si256(l, _mm256_permute2x128_si256(h, h, 1));
+    let mask0 = unsafe { std::mem::transmute::<__m256i, Mask<i64, 4>>(mask0).to_bitmask() as u8 };
+
+    let l = _mm256_or_si256(m00, m10);
+    let h = _mm256_or_si256(m01, m11);
+    let mask1 = _mm256_or_si256(l, _mm256_shuffle_epi32(h, _MM_PERM_BADC));
+    let mask1 = unsafe { std::mem::transmute::<__m256i, Mask<i64, 4>>(mask1).to_bitmask() as u8 };
+    (mask0, mask1)
+}
+
 fuzz_target!(|v: ([i64; 8], [i64; 8])| {
     unsafe {
         let a = _mm512_loadu_epi64(&v.0 as *const _);
         let b = _mm512_loadu_epi64(&v.1 as *const _);
-        assert_eq!(emulate_vp2intersectq(a, b), native_vp2intersectq(a, b));
+        assert_eq!(emulate_vp2intersectq_avx512(a, b), native_vp2intersectq_avx512(a, b));
+
+        let a = _mm256_loadu_si256(&v.0 as *const _ as *const _);
+        let b = _mm256_loadu_si256(&v.1 as *const _ as *const _);
+        assert_eq!(emulate_vp2intersectq_avx2(a, b), native_vp2intersectq_avx2(a, b));
     }
 });
