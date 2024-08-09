ca19a4c2fee584c1922e83760daaea54c8cfea7b
Gabriel Menezes <gaha204@hotmail.com>
2024-08-09 01:07:11 -0300

    clean main

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,20 @@ use rkyv::{
     Serialize,
 };
 use std::{
-    arch::{asm, x86_64::{__m512i, __mmask8, _mm512_loadu_epi64}}, fmt::{Debug, Display}, fs::File, hint::black_box, io::{BufRead, BufReader}, iter::Map, mem::MaybeUninit, path::PathBuf, process::{Command, Stdio}, simd::{cmp::SimdPartialEq, u64x8, Mask, Simd}, sync::atomic::{AtomicU32, Ordering::Relaxed}
+    arch::{
+        asm,
+        x86_64::{__m512i, __mmask8, _mm512_loadu_epi64},
+    },
+    fmt::{Debug, Display},
+    fs::File,
+    hint::black_box,
+    io::{BufRead, BufReader},
+    iter::Map,
+    mem::MaybeUninit,
+    path::PathBuf,
+    process::{Command, Stdio},
+    simd::{cmp::SimdPartialEq, u64x8, Mask, Simd},
+    sync::atomic::{AtomicU32, Ordering::Relaxed},
 };
 
 #[derive(Parser, Debug)]
@@ -104,10 +117,10 @@ where
 
             let b = std::time::Instant::now();
 
-            // searcher.foo(q);
             let doc_ids = searcher.search(q, &stats);
             // let doc_ids = searcher.par_search(q, &stats);
             // let doc_ids = shard.search(q, &stats);
+
             // println!("{doc_ids:?}");
             // let doc_ids = db.get_documents_by_ids(doc_ids);
 
@@ -224,237 +237,7 @@ fn index_msmarco(args: IndexFile) {
     );
 }
 
-#[inline(never)]
-unsafe fn intersect(lhs: &[u64], rhs: &[u64]) -> Vec<u64> {
-    #[inline(always)]
-    unsafe fn word_wise_check<F: Fn(u64) -> u64>(
-        a: &[u64],
-        lhs_i: usize,
-        b: &[u64],
-        rhs_i: usize,
-        op: F,
-    ) -> Mask<i32, 16> {
-        let a0 = op(*a.get_unchecked(lhs_i + 0)) as u32;
-        let a1 = op(*a.get_unchecked(lhs_i + 1)) as u32;
-        let a2 = op(*a.get_unchecked(lhs_i + 2)) as u32;
-        let a3 = op(*a.get_unchecked(lhs_i + 3)) as u32;
-        let amask = Simd::from_array([
-            a0, a0, a0, a0, a1, a1, a1, a1, a2, a2, a2, a2, a3, a3, a3, a3,
-        ]);
-
-        let b0 = op(*b.get_unchecked(rhs_i + 0)) as u32;
-        let b1 = op(*b.get_unchecked(rhs_i + 1)) as u32;
-        let b2 = op(*b.get_unchecked(rhs_i + 2)) as u32;
-        let b3 = op(*b.get_unchecked(rhs_i + 3)) as u32;
-        let bmask = Simd::from_array([
-            b0, b1, b2, b3, b0, b1, b2, b3, b0, b1, b2, b3, b0, b1, b2, b3,
-        ]);
-
-        amask.simd_eq(bmask)
-    }
-
-    #[inline(always)]
-    fn cmovnz(value: &mut MaybeUninit<u64>, v: &u64, condition: u8) {
-        unsafe {
-            asm! {
-                "test {0}, {0}",
-                "cmovnz {1:r}, {2:r}",
-                in(reg_byte) condition,
-                inlateout(reg) *value,
-                in(reg) *v,
-                options(pure, nomem, nostack),
-            };
-        }
-    }
-
-    let min = lhs.len().min(rhs.len());
-    let mut results: Box<[MaybeUninit<u64>]> = Box::new_uninit_slice(min);
-    let mut i = 0;
-
-    let mut lhs_i = 0;
-    let mut rhs_i = 0;
-
-    let end_lhs = lhs.len() / 4 * 4;
-    let end_rhs = rhs.len() / 4 * 4;
-    let a = lhs.get_unchecked(..end_lhs);
-    let b = rhs.get_unchecked(..end_rhs);
-    while lhs_i < a.len() && rhs_i < b.len() {
-        let lsb = word_wise_check(a, lhs_i, b, rhs_i, |v| v).to_bitmask();
-        if lsb >= 1 {
-            // this if is very likely to happend
-
-            let msb = word_wise_check(a, lhs_i, b, rhs_i, |v| (v & 0xFFFFFFFF00000000) >> 32)
-                .to_bitmask();
-            // a0, a0, a0, a0 | a1, a1, a1, a1 | a2, a2, a2, a2 | a3, a3, a3, a3
-            // b0, b1, b2, b3 | b0, b1, b2, b3 | b0, b1, b2, b3 | b0, b1, b2, b3
-            let bitmask = lsb & msb;
-            let a0b0123 = ((bitmask & 0b0000_0000_0000_1111) > 0) as u8;
-            let a1b0123 = ((bitmask & 0b0000_0000_1111_0000) > 0) as u8;
-            let a2b0123 = ((bitmask & 0b0000_1111_0000_0000) > 0) as u8;
-            let a3b0123 = ((bitmask & 0b1111_0000_0000_0000) > 0) as u8;
-
-            let a0 = a.get_unchecked(lhs_i + 0);
-            let a1 = a.get_unchecked(lhs_i + 1);
-            let a2 = a.get_unchecked(lhs_i + 2);
-            let a3 = a.get_unchecked(lhs_i + 3);
-
-            let b3 = b.get_unchecked(rhs_i + 3);
-
-            cmovnz(results.get_unchecked_mut(i), a0, a0b0123);
-            i += a0b0123 as usize;
-            cmovnz(results.get_unchecked_mut(i), a1, a1b0123);
-            i += a1b0123 as usize;
-            cmovnz(results.get_unchecked_mut(i), a2, a2b0123);
-            i += a2b0123 as usize;
-            cmovnz(results.get_unchecked_mut(i), a3, a3b0123);
-            i += a3b0123 as usize;
-
-            // branching in this case seems to be the fastest
-            match a3.cmp(b3) {
-                std::cmp::Ordering::Greater => rhs_i += 4,
-                std::cmp::Ordering::Less => lhs_i += 4,
-                std::cmp::Ordering::Equal => {
-                    lhs_i += 4;
-                    rhs_i += 4;
-                }
-            }
-        } else if *a.get_unchecked(lhs_i + 3) > *b.get_unchecked(rhs_i + 3) {
-            // we want to avoid this else if from running, since it's almost
-            // a 50-50%, making it hard to predict, for this reason we repeat
-            // this same check inside the
-            rhs_i += 4;
-        } else {
-            lhs_i += 4;
-        }
-    }
-
-    // for the remaining elements we can do a 2 pointer approach
-    // since the 2 slices will be small
-    while lhs_i < lhs.len() && rhs_i < rhs.len() {
-        if lhs[lhs_i] == rhs[rhs_i] {
-            results.get_unchecked_mut(i).write(lhs[lhs_i]);
-            i += 1;
-            lhs_i += 1;
-            rhs_i += 1;
-        } else if lhs[lhs_i] > rhs[rhs_i] {
-            rhs_i += 1;
-        } else {
-            lhs_i += 1;
-        }
-    }
-
-    unsafe { Vec::from_raw_parts(Box::into_raw(results) as *mut _, i, min) }
-}
-
-fn naive(lhs: &[u64], rhs: &[u64]) -> Vec<u64> {
-    let min = lhs.len().min(rhs.len());
-    let mut results: Box<[MaybeUninit<u64>]> = Box::new_uninit_slice(min);
-    let mut i = 0;
-
-    let mut lhs_i = 0;
-    let mut rhs_i = 0;
-
-    while lhs_i < lhs.len() && rhs_i < rhs.len() {
-        if lhs[lhs_i] == rhs[rhs_i] {
-            unsafe { results.get_unchecked_mut(i).write(lhs[lhs_i]) };
-            i += 1;
-            lhs_i += 1;
-            rhs_i += 1;
-        } else if lhs[lhs_i] > rhs[rhs_i] {
-            rhs_i += 1;
-        } else {
-            lhs_i += 1;
-        }
-    }
-    unsafe { Vec::from_raw_parts(Box::into_raw(results) as *mut _, i, min) }
-}
-
-fn gallop(lhs: &[u64], rhs: &[u64]) -> Vec<u64> {
-    let min = lhs.len().min(rhs.len());
-    let mut results: Box<[MaybeUninit<u64>]> = Box::new_uninit_slice(min);
-    let mut i = 0;
-
-    let mut lhs_i = 0;
-    let mut rhs_i = 0;
-
-    while lhs_i < lhs.len() && rhs_i < rhs.len() {
-        let mut lhs_delta = 1;
-        let mut rhs_delta = 1;
-
-        while lhs_i < lhs.len() && lhs[lhs_i] < rhs[rhs_i] {
-            lhs_i += lhs_delta;
-            lhs_delta *= 2;
-        }
-        lhs_i -= lhs_delta / 2;
-
-        while rhs_i < rhs.len() && rhs[rhs_i] < unsafe { *lhs.get_unchecked(lhs_i) } {
-            rhs_i += rhs_delta;
-            rhs_delta *= 2;
-        }
-        rhs_i -= rhs_delta / 2;
-
-        let lhs = unsafe { lhs.get_unchecked(lhs_i) };
-        let rhs = unsafe { rhs.get_unchecked(rhs_i) };
-        match lhs.cmp(rhs) {
-            std::cmp::Ordering::Less => lhs_i += 1,
-            std::cmp::Ordering::Greater => rhs_i += 1,
-            std::cmp::Ordering::Equal => {
-                unsafe { results.get_unchecked_mut(i).write(*lhs) };
-                i += 1;
-                lhs_i += 1;
-                rhs_i += 1;
-            }
-        }
-    }
-    unsafe { Vec::from_raw_parts(Box::into_raw(results) as *mut _, i, min) }
-}
-
 fn main() {
-    // let mut rng = rand::thread_rng();
-    // loop {
-    //     let v0_len = rng.gen_range(500000usize..20000000);
-    //     let mut v0: Vec<_> = (0..v0_len).map(|_| rng.gen_range(0u64..40000000)).collect();
-
-    //     let v1_len = rng.gen_range(500000usize..20000000);
-    //     let mut v1: Vec<_> = (0..v1_len).map(|_| rng.gen_range(0u64..40000000)).collect();
-
-    //     v0.sort_unstable();
-    //     v1.sort_unstable();
-    //     v0.dedup();
-    //     v1.dedup();
-
-    //     const ITERS: usize = 128;
-    //     let b = std::time::Instant::now();
-    //     for _ in 0..ITERS {
-    //         std::hint::black_box(unsafe { intersect(&v0, &v1) });
-    //     }
-    //     let t_intersect = b.elapsed().as_millis() as f64 / ITERS as f64;
-
-    //     let b = std::time::Instant::now();
-    //     for _ in 0..ITERS {
-    //         std::hint::black_box(naive(&v0, &v1));
-    //     }
-    //     let t_naive = b.elapsed().as_millis() as f64 / ITERS as f64;
-
-    //     let b = std::time::Instant::now();
-    //     for _ in 0..ITERS {
-    //         std::hint::black_box(gallop(&v0, &v1));
-    //     }
-    //     let t_gallop = b.elapsed().as_millis() as f64 / ITERS as f64;
-
-    //     println!(
-    //         "i/n: {:.4} | i/g: {:.4} | n/g: {:.4}",
-    //         t_intersect / t_naive,
-    //         t_intersect / t_gallop,
-    //         t_naive / t_gallop
-    //     );
-    //     let int = unsafe { intersect(&v0, &v1) };
-    //     let nai = naive(&v0, &v1);
-    //     let gal = gallop(&v0, &v1);
-    //     assert_eq!(int, nai, "{v0:?}\n\n{v1:?}");
-    //     assert_eq!(int, gal, "{v0:?}\n\n{v1:?}");
-    // }
-    // return;
     let args = CommandArgs::parse();
 
     // spawn rayon threads to avoid unecessary respawn
