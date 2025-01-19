e45a27e4cd59592ec9b175083f575ac7edf3e44d
Gabriel <gaha204@hotmail.com>
2025-01-19 17:12:03 -0300

    save some stuff

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,21 @@
 #![feature(allocator_api)]
 #![feature(iter_intersperse)]
 #![feature(new_zeroed_alloc)]
+#![feature(file_buffered)]
 
 // use arrow::array::{Int32Array, StringArray};
 // use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 use clap::{Args, Parser, Subcommand, ValueEnum};
+// use serde::Serialize as SerdeSerialize;
 use core::str;
-use phrase_search::{simd::SimdIntersect, CommonTokens, Indexer, Searcher, Stats};
+use phrase_search::{naive::NaiveIntersect, simd::SimdIntersect, tokenize, CommonTokens, Indexer, Intersect, Searcher, Stats};
 use rkyv::{
     api::high::HighSerializer, ser::allocator::ArenaHandle, util::AlignedVec, Archive, Serialize,
 };
 use std::{
     fmt::Debug,
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, BufWriter, Write},
     path::PathBuf,
 };
 
@@ -116,80 +118,6 @@ where
     }
 }
 
-// fn index_text(args: IndexFolder) {
-//     println!("Start Indexing");
-
-//     let files: Vec<_> = std::fs::read_dir(&args.folder)
-//         .unwrap()
-//         .map(|file| file.unwrap().path())
-//         .collect();
-
-//     println!();
-//     println!("Files to Index: {files:#?}");
-//     println!();
-
-//     let docs: Vec<_> = files
-//         .into_iter()
-//         .map(|file| {
-//             let content = std::fs::read_to_string(&file).unwrap();
-//             (content, file.to_str().unwrap().to_owned())
-//         })
-//         .collect();
-
-//     let b = std::time::Instant::now();
-
-//     // let mut indexer = Indexer::new(&args.index_name, args.db_size, None, None, None);
-//     let mut indexer = Indexer::new(&args.index_name, args.db_size, None, None, None);
-//     let num_docs = indexer.index(docs);
-//     indexer.flush();
-
-//     println!(
-//         "Whole Indexing took {:?} ({num_docs} documents)",
-//         b.elapsed()
-//     );
-// }
-
-// fn index_parquet(args: IndexFolder) {
-//     println!("Start Indexing");
-
-//     let files: Vec<_> = std::fs::read_dir(&args.folder)
-//         .unwrap()
-//         .map(|file| file.unwrap().path())
-//         .collect();
-
-//     let mut docs = Vec::new();
-//     for file in files.iter() {
-//         let file = File::open(file).unwrap();
-//         let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
-
-//         let reader = builder.build().unwrap();
-
-//         for record_batch in reader {
-//             let record_batch = record_batch.unwrap();
-//             let query_id = record_batch.column(3);
-//             let query_id = query_id.as_any().downcast_ref::<Int32Array>().unwrap();
-//             let queries = record_batch.column(2);
-//             let queries = queries.as_any().downcast_ref::<StringArray>().unwrap();
-//             for (query_id, query) in query_id.iter().zip(queries.iter()) {
-//                 docs.push((query.unwrap().to_owned(), query_id.unwrap()));
-//             }
-//         }
-//     }
-
-//     println!();
-//     println!("Files to Index: {files:#?} ({} documents)", docs.len());
-//     println!();
-
-//     let b = std::time::Instant::now();
-//     let next_doc_id = AtomicU32::new(0);
-
-//     let db = DB::truncate(&args.index_name, args.db_size);
-//     let indexer = db.indexer(&next_doc_id);
-//     indexer.index(docs);
-
-//     println!("Whole Indexing took {:?}", b.elapsed());
-// }
-
 fn index_msmarco(args: IndexFile) {
     println!("Start Indexing");
 
@@ -211,9 +139,92 @@ fn index_msmarco(args: IndexFile) {
     );
 }
 
+fn benchmark<D>(args: Search)
+where
+    D: for<'a> Serialize<HighSerializer<AlignedVec, ArenaHandle<'a>, rkyv::rancor::Error>>
+        + Archive
+        + Send
+        + Sync
+        + 'static,
+{
+    fn inner<D, I>(runs: u32, q: &str, searcher: &Searcher<D>) -> (f64, usize)
+    where
+        D: for<'a> Serialize<HighSerializer<AlignedVec, ArenaHandle<'a>, rkyv::rancor::Error>>
+        + Archive
+        + Send
+        + Sync
+        + 'static,
+        I: Intersect
+    {
+        let stats = Stats::default();
+        for _ in 0..20 {
+            let doc_ids = searcher.search::<I>(q, &stats);
+            std::hint::black_box(doc_ids);
+        }
+
+        let stats = Stats::default();
+        let b = std::time::Instant::now();
+        for _ in 0..runs {
+            let doc_ids = searcher.search::<I>(q, &stats);
+            std::hint::black_box(doc_ids);
+        }
+        let e = b.elapsed().as_micros();
+        let avg_ms = e as f64 / runs as f64 / 1000_f64;
+
+        let stats_ = Stats::default();
+        let n_found = searcher.search::<I>(q, &stats_).len();
+
+        (avg_ms, n_found)
+    }
+
+    let searcher = Searcher::<D>::new(&args.index_name, args.db_size).unwrap();
+
+    let queries = std::fs::read_to_string(args.queries).unwrap();
+    let queries: Vec<_> = queries.lines().collect();
+    for q in queries.iter() {
+        let (t0, r0) = inner::<_, SimdIntersect>(args.runs, q, &searcher);
+        let (t1, r1) = inner::<_, NaiveIntersect>(args.runs, q, &searcher);
+        assert_eq!(r0, r1);
+        println!("{q} | {t0:.4} | {t1:.4} | {r0}");
+    }
+}
+
 fn main() {
+    // #[derive(SerdeSerialize)]
+    // struct Doc<'a> {
+    //     id: u32,
+    //     content: &'a str
+    // }
+
+    // let file = File::create("./docs.jsonl").unwrap();
+    // let mut writer = BufWriter::with_capacity(32 * 1024, file);
+    // let reader = BufReader::new(File::open("./fulldocs.tsv").unwrap());
+    // for (id, line) in reader.lines().enumerate() {
+    //     let line = line.unwrap();
+    //     let mut it = line.split("\t");
+    //     it.next().unwrap();
+    //     it.next().unwrap();
+
+    //     let content = it.next().unwrap();
+    //     let doc = Doc {
+    //         content,
+    //         id: id as u32
+    //     };
+    //     serde_json::to_writer(&mut writer, &doc).unwrap();
+    //     writer.write_all(&[b'\n']).unwrap();
+    //     if id % 50_000 == 0{
+    //         println!("{id}");
+    //     }
+    // }
+
+    // writer.flush().unwrap();
+
+    // return;
     //0.0002f64
     let args = CommandArgs::parse();
+
+    println!("Query             | Simd    | Naive  | Results");
+    println!("------------------| --------| -------| -------");
 
     // spawn rayon threads to avoid unecessary respawn
     // rayon::ThreadPoolBuilder::new().build_global().unwrap();
@@ -225,7 +236,8 @@ fn main() {
         Ty::Search(arg) => match arg.data_set {
             DataSet::Text => search::<String>(arg),
             DataSet::Parquet => search::<i32>(arg),
-            DataSet::MsMarco => search::<u32>(arg),
+            // DataSet::MsMarco => search::<u32>(arg),
+            DataSet::MsMarco => benchmark::<u32>(arg),
         },
     }
 }
---
+++ types/intersection.md
@@ -0,0 +1,32 @@
+Query             | Simd    | Naive  | Results
+------------------| --------| -------| -------
+programming languages | 0.1496 | 0.7454 | 3284
+you need to be | 1.7760 | 9.3750 | 16144
+chemistry lessons | 0.1017 | 0.5944 | 38
+how to file taxes | 0.4956 | 2.1627 | 32
+how to make money | 2.0469 | 10.2508 | 779
+and you are in the most | 0.0266 | 0.1630 | 0
+as well as our | 0.3715 | 1.1343 | 2031
+you should go to the | 0.2172 | 1.3700 | 564
+do you know ? | 0.1869 | 0.8600 | 1054
+http:// | 2.1016 | 5.7091 | 280110
+if i was to give you | 0.1602 | 1.2111 | 3
+may have significant health effects | 0.4302 | 3.2684 | 3
+where are you going | 0.0168 | 0.0295 | 602
+but refused to join in | 0.0358 | 0.1139 | 2
+it may be useful to | 5.9048 | 13.7519 | 961
+A logical step before choosing the metrics and activities | 0.0228 | 0.0806 | 1
+of American Universities | 0.0854 | 0.6251 | 244
+beta-plus decay, it changes | 0.0107 | 0.0176 | 1
+https://google | 0.0932 | 0.2599 | 0
+However, some argue that it has less taste | 0.0483 | 0.2504 | 1
+It is important not to | 0.2090 | 1.4102 | 1564
+Windows 10 | 1.6637 | 7.5760 | 11264
+knowledge base article | 0.6087 | 3.3039 | 440
+is common in | 14.0448 | 32.1616 | 10134
+this is common in | 0.5524 | 2.3168 | 522
+this is common in the | 0.5166 | 1.3803 | 57
+and is thought to be | 0.2677 | 1.1394 | 673
+, you need to make sure that you have | 0.7306 | 2.6941 | 29
+we hope you had a great time | 0.1882 | 0.8915 | 4
+phrase search | 0.2445 | 1.3501 | 9
